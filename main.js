2af6a8914814085edf00aee4f7fa010fe80acec2
romanpast <pastroman98@gmail.com>
2022-10-18 23:51:30 +0900

    test

--- main.js
+++ main.js
@@ -12,6 +12,26 @@ mobileSize.addEventListener("change", () => {
 
 
 // slider
+const entities = [
+    {
+        cityTitle: "Rostov-on-Don LCD admiral",
+        aptArea: "81 m2",
+        repairTime: "3.5 months"
+    },
+
+    {
+        cityTitle: "Sochi Thieves",
+        aptArea: "105 m2",
+        repairTime: "4 months" 
+    },
+
+    {
+        cityTitle: "Rostov-on-Don Patriotic",
+        aptArea: "93 m2",
+        repairTime: "3 months"
+    }
+]
+
 
 
 
