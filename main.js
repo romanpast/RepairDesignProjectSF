bd3e3eab6f56c385894c493a1ec67cf2c48d5030
romanpast <pastroman98@gmail.com>
2022-10-19 14:31:16 +0900

    test

--- main.js
+++ main.js
@@ -12,26 +12,70 @@ mobileSize.addEventListener("change", () => {
 
 
 // slider
-const entities = [
-    {
-        cityTitle: "Rostov-on-Don LCD admiral",
+const entities = [{
+        city: "Rostov-on-Don LCD admiral",
         aptArea: "81 m2",
         repairTime: "3.5 months"
     },
 
     {
-        cityTitle: "Sochi Thieves",
+        city: "Sochi Thieves",
         aptArea: "105 m2",
-        repairTime: "4 months" 
+        repairTime: "4 months"
     },
 
     {
-        cityTitle: "Rostov-on-Don Patriotic",
+        city: "Rostov-on-Don Patriotic",
         aptArea: "93 m2",
         repairTime: "3 months"
     }
 ]
 
+let cityTitle = document.querySelector("#cityTitle")
+
+
+
+// old
+
+// let text = document.querySelector('.text')
+// let pagination = document.querySelectorAll('.ball');
+// let carousel = document.getElementById('carousel');
+// let cards = document.querySelectorAll('.card');
+// let leftBtn = document.getElementById('leftArrow');
+// let rightBtn = document.getElementById('rightArrow');
+// let section = 0
+
+// const updatePag = () => {
+//   pagination.forEach((el) => el.style.backgroundColor = 'rgba(255, 255, 255, 0.3)')
+//   pagination[section].style.backgroundColor = 'white'
+// }
+
+// pagination.forEach((curItem, index) => {
+//   curItem.addEventListener('click', () => {
+//     section = index;
+//     updatePag()
+//     carousel.scrollLeft = (index * (window.innerWidth - 150)) + (index * 10)
+
+//     text.innerText = titles[section].title
+//   })
+// })
+
+// leftBtn.addEventListener('click', () => {
+//   section == 0 ? section = 2 : section--;
+//   carousel.scrollLeft = (section * (window.innerWidth - 150)) + (section * 10)
+//   updatePag();
+//   text.innerText = titles[section].title
+// })
+
+
+// rightBtn.addEventListener('click', () => {
+//     section == 2 ? section = 0 : section++;
+//     carousel.scrollLeft = (section * (window.innerWidth - 150)) + (section * 10)
+//     updatePag();
+//     text.innerText = titles[section].title
+// })
+
+// old
 
 
 
@@ -42,4 +86,4 @@ const updatePag = () => {
     underline.forEach((el) => el.style.border = '#131313');
     pagination[section].style.backgroundColor = 'white';
     underline[section].style.border = '1px solid #E3B873';
-  }
+}
