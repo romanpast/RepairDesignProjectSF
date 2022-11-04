012de36169648ff9890359dc62bf9e4cec751b33
romanpast <pastroman98@gmail.com>
2022-11-04 22:39:14 +0900

    gulp modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,29 @@
 const gulp = require('gulp');
 const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer-core');
+const autoprefixer = require('autoprefixer');
 const sourcemaps = require('gulp-sourcemaps')
+const color_rgba_fallback = require('postcss-color-rgba-fallback');
+const opacity = require('postcss-opacity');
+const pseudoelements = require('postcss-pseudoelements');
+const vmin = require('postcss-vmin');
+const pixrem = require('pixrem');
+const will_change = require('postcss-will-change');
 
+gulp.task('modify', () => {
 
-gulp.task('modify', () => {    
-    
-        return gulp.src('public/src/style.css')
-          .pipe(sourcemaps.init())
-          .pipe(postcss([autoprefixer]))
+      const processors = [
+        will_change,
+        autoprefixer,
+        color_rgba_fallback,
+        opacity,
+        pseudoelements,
+        vmin,
+        pixrem
+      ];
+
+      return gulp.src('public/src/style.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss(processors))
           .pipe(sourcemaps.write('.'))
           .pipe(gulp.dest('public/css'))
-      })
+        })
