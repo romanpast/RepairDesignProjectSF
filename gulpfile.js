1e29ac655ebb108bb26a9446418cda11fd347b4f
romanpast <pastroman98@gmail.com>
2022-11-08 10:41:52 +0900

    corrections

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,19 @@ const color_rgba_fallback = require('postcss-color-rgba-fallback');
 const opacity = require('postcss-opacity');
 const pseudoelements = require('postcss-pseudoelements');
 const vmin = require('postcss-vmin');
-const postcssPresetEnv = require('postcss-preset-env');
+const pixrem = require('pixrem');
+const will_change = require('postcss-will-change');
 
 gulp.task('modify', () => {
 
       const processors = [
+        will_change,
         autoprefixer,
+        color_rgba_fallback,
+        opacity,
         pseudoelements,
         vmin,
-        postcssPresetEnv
+        pixrem
       ];
 
       return gulp.src('public/src/style.css')
